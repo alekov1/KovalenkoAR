ce155e98054cf9ef9c76f627aea36a6625a88892
Alekov <145697230+alekov1@users.noreply.github.com>
2023-11-01 18:56:25 +0300

    Update 8.py

--- Lesson5/8.py
+++ Lesson5/8.py
@@ -1,13 +1,17 @@
-n = int(input())
-k = []
-k.append(n)
-while n != 0:
-    n = int(input())
+
+def numbers(n):
+    k = []
     k.append(n)
-count = 1
-maxcount = 0
-for x in range(1, len(k)):
-    if k[x] == k[x-1]:
-        count += 1
-        maxcount = max(maxcount, count)
-print(maxcount)
+    while n != 0:
+        n = int(input())
+        k.append(n)
+    count = 1
+    maxcount = 0
+    for x in range(1, len(k)):
+        if k[x] == k[x-1]:
+            count += 1
+            maxcount = max(maxcount, count)
+    return maxcount
+
+n = int(input())
+print(numbers(n))
