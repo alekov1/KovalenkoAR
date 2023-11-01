793367987c0890b6a274928a57b64818c4ebf850
Alekov <145697230+alekov1@users.noreply.github.com>
2023-11-01 18:52:09 +0300

    Update 7.py

--- Lesson5/7.py
+++ Lesson5/7.py
@@ -1,8 +1,11 @@
-n = -999999999999999
-count = 0
-while n != 0:
-    n_last = n
-    n = int(input())
-    if n > n_last:
-        count += 1
-print(count - 1)
+def numbers(x):
+    x = 1
+    n = -999999999999999
+    count = 0
+    while n != 0:
+        n_last = n
+        n = int(input())
+        if n > n_last:
+            count += 1
+    return count - 1
+print(numbers(1))
