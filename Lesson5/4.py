8ae08905e3e62495091c731d34fea73cfb6ff221
Alekov <145697230+alekov1@users.noreply.github.com>
2023-11-01 18:37:27 +0300

    Update 4.py

--- Lesson5/4.py
+++ Lesson5/4.py
@@ -1,8 +1,11 @@
+def Numbers(x, y):
+    count_day = 1
+    while x < y:
+        x = x * 1.1
+        count_day += 1
+    return count_day
+
 x = int(input())
 y = int(input())
-count_day = 1
 
-while x < y:
-    x = x * 1.1
-    count_day += 1
-print(count_day)
+print(Numbers(x,y))
