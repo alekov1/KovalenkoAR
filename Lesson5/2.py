8f63c1de853f10427698a0af8f32e27b54d7514b
Alekov <145697230+alekov1@users.noreply.github.com>
2023-11-01 18:32:11 +0300

    Update 2.py

--- Lesson5/2.py
+++ Lesson5/2.py
@@ -1,5 +1,8 @@
+def afwfwaf(N):
+    if N < 2:
+        print('error')
+    N1 = [int(x) for x in range(2, N + 1) if N % x == 0]
+    return min(N1)
+
 N = int(input())
-if N < 2:
-    print('error')
-N1 = [int(x) for x in range(2, N+1) if N % x == 0]
-print(min(N1))
+print(afwfwaf(N))
