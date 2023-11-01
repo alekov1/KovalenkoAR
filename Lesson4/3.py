74db8d62d72c7bd0cb3c8ff8b619fbdfab942162
Alekov <145697230+alekov1@users.noreply.github.com>
2023-11-01 18:00:17 +0300

    Update 3.py

--- Lesson4/3.py
+++ Lesson4/3.py
@@ -1,11 +1,12 @@
-A = int(input())
-B = int(input())
-
-if A > B:
-    while A != B-1:
-        x = A
-        if x % 2 != 0:
-            print(A)
-            A -= 1
-        else:
-            A -= 1
+def numbers(A: int, B: int):
+    ListOfNumbers = []
+    if A > B:
+        while A != B-1:
+            x = A
+            if x % 2 != 0:
+                ListOfNumbers.append(A)
+                A -= 1
+            else:
+                A -= 1
+    return ListOfNumbers
+print(numbers(15, 1))
