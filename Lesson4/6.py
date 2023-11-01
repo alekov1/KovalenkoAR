ffedc6f50bc17d5b71a88bbcfa0a532520c35a3a
Alekov <145697230+alekov1@users.noreply.github.com>
2023-11-01 18:21:46 +0300

    Update 6.py

--- Lesson4/6.py
+++ Lesson4/6.py
@@ -1,6 +1,12 @@
+def Numbers(N):
+    S = [int(x) for x in range(1, N+1)]
+    factor_N = 1
+    ListOfFactorN = []
+    for y in range(len(S)):
+            factor_N = factor_N * S[y]
+    ListOfFactorN.append(factor_N)
+    return ListOfFactorN
+0
+
 N = int(input())
-S = [int(x) for x in range(1, N+1)]
-factor_N = 1
-for y in range(len(S)):
-        factor_N = factor_N * S[y]
-print(factor_N)
+print(Numbers(N))
